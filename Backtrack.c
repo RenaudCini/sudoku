115409ed6caab94a28de851a56ec841e2ef5d3a0
renaud <renaudcini@gmail.com>
2019-01-04 15:33:33 +0100

    master

---
+++ Backtrack.c
@@ -0,0 +1,54 @@
+/ * *********************************************** *************************** * /
+/ *                                                                             * /
+/ *                                                         ::: ::::::::    * * /
+/ *    backtrack.c: +:: +:: +:    * /
+/ *                                                     +: + +: + +: +      * /
+/ *    Par: Renaudcini <renaudcini@gmail.com> + # + +: + + # +         * /
+/ *                                                 + # + # + # + # + # + + # +            * /
+/ *    Créé: 2014/09/06 16:44:10 par Renaudcini # + # # + #              * /
+/ *    Mise à jour: 2014/09/07 15:13:56 par Renaudcini ### ########. Fr        * /
+/ *                                                                             * /
+/ * *********************************************** *************************** * /
+
+// #include <stdlib.h>
+
+/ * void ** tab_fill (int ac, char ** av, int tab [8] [8])
+{
+	int i;
+	int j;
+	i = 0;
+	j = 0;
+	tant que (i <8)
+	{
+		tandis que (i <ac - 1)
+		{
+			onglet [i] [j] = av [i] [j];
+			j ++;
+		}
+		i ++;
+	}
+	retour (onglet [8] [8]);
+}
+annulation de retour (caractère ** onglet)
+{
+	int i;
+	int j;
+	i = 0;
+	tant que (i <8)
+	{
+		j = 0;
+		tant que (j <8)
+		{
+			if (test_value (tab * i * j == 1);
+				revenir en arrière (onglet);
+			j ++;
+		}
+		i ++;
+	}
+	retourner (0);
+}
+int testvalue (car n)
+{
+	int fit;
+	retour (ajustement)
+}
---
+++ ft.h
@@ -0,0 +1,24 @@
+/ * *********************************************** *************************** * /
+/ *                                                                             * /
+/ *                                                         ::: ::::::::    * * /
+/ *   ft.h: +:: +:: +:    * /
+/ *                                                     +: + +: + +: +      * /
+/ *    Par: Renaudcini <renaudcini@gmail.com> + # + +: + + # +         * /
+/ *                                                 + # + # + # + # + # + + # +            * /
+/ *    Créé: 2014/09/06 16:44:10 par Renaudcini # + # # + #              * /
+/ *    Mise à jour: 2014/09/07 15:13:56 par Renaudcini ### ########. Fr        * /
+/ *                                                                             * /
+/ * *********************************************** *************************** * /
+
+
+#ifndef	FT_H
+# define	FT_H
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_putnbr(int n);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+void	ft_swap(int *a, int *b);
+void	ft_tab_fill(char **tab);
+void	ft_display(char **tab);
+#endif
